7eda6fd0de2b678d020ac2a012a01e1471ce791a
Ashik Mridha <ashikmridha43@gmail.com>
2026-03-08 23:58:16 +0600

    added navber sticky top

--- script/home.js
+++ script/home.js
@@ -20,8 +20,10 @@ const getPriorityColor = (priority) => {
 const getLabelColor = (label) => {
     label = label.toLowerCase();
     if (label === 'bug') return 'badge-error';
-    if (label === 'feature') return 'badge-success';
-    if (label === 'enhancement') return 'badge-warning';
+    if (label === 'feature') return 'badge-secondery';
+    if (label === 'enhancement') return 'badge-primary';
+    if (label === 'documentation') return 'badge-secondary';
+    if (label === 'question') return 'badge-primary';
     return 'badge-info';
 }
 
@@ -63,32 +65,53 @@ const renderIssues = (issues) => {
     issueCount.textContent = issues.length;
 
     issues.forEach(issue => {
+        const labelsHTML = Array.isArray(issue.labels) ? issue.labels.map(label => {
+            const colorClass = getLabelColor(label);
+            return `<span class="budge budge-outline ${colorClass} bg-yellow-50 border-yellow-100 rounded-full gap-1 py-1 px-3 text-[9px] font-extrabold uppercase border-2">
+            <i class="fa-solid fa-bug text-[5px] md:text-[6px]"></i> ${label}
+            </span>
+            `
+        })
+            .join('') : '';
+
         const card = document.createElement('div');
-        card.className = `bg-white p-3 rounded shadow-sm border-t-4 cursor-pointer hover:shadow-md transition
-         ${issue.status === 'open' ? 'border-green-500' : 'border-purple-500'}`
+        card.className = `
+        bg-white p-4 md:p-5 rounded-xl shadow-sm border-t-[6px] cursor-pointer hover:shadow-md transition flex flex-col justify-betweenh-full
+        ${issue.status === 'open' ? 'border-green-500' : 'border-purple-500'}
+        `;
 
         card.innerHTML = `
-        
-        <div class="flex items-center gap-2 mb-2">
+        <div>
+            <div class="flex justify-between items-start mb-3 md:mb-4">
 
-            <div class="flex gap-4 items-center justify-between w-full">
-               <span class="p-2 bg-green-100 rounded-full"><i class="fa-solid fa-spinner"></i></span>
-               <span class="badge ${getPriorityColor(issue.priority)}">${issue.priority}</span>
+            <div class="w-8 h-8 md:w-9 md:h-9 rounded-full bg-green-100 flex items-center justify-center">
+            <i class="fa-solid fa-spinner text-gray-400 text-xs md:text-sm"></i>
             </div>
+
+            <span class="badge ${getPriorityColor(issue.priority)} border-none h-6 md:h-7 px-3  md:px-4 text-[9px] md:text-[10px] font-bold uppercase text-white tracking-wider">
+                ${issue.priority}
+                </span>
+
+            </div>
+
+            <h3 class="text-base md:text-lg font-bold mb-2 leading-snug text-gray-800">
+                <span class="line-clamp-2">${issue.title}</span>
+            </h3>
+
+            <p class="text-xs md:text-sm text-gray-500 mb-4 md:mb-5 line-clamp-3 leading-relaxed">
+            ${issue.description}
+            </p>
         </div>
 
-         <h3 class="text-lg font-bold mb-1">
-            <span class="line-clamp-2">${issue.title}</span>
-         </h3>
 
-         <p class="text-xs md:text-sm text-gray-600 mb-3 line-clamp-3">${issue.description}</p>
-
-         <div class="flex items-start flex-col flex-wrap justify-between gap-2">
-            <div class="badge ${getLabelColor}">${issue.labels}</div>
+        <div class="mt-auto">
+            <div class="flex flex-wrap gap-1.5 md:gap-2 mb-4 md:mb-5">
+            ${labelsHTML}
+            </div>
                 
-            <div>
-            <span class="text-[12px] md:text-xs text-gray-400">#${issue.author}</span>
-            <span class="text-[10px] md:text-xs text-gray-400">${new Date(issue.createdAt).toLocaleDateString()}</span>
+            <div class="flex justify-between items-center pt-3 md:pt-4 border-t border-gray-100">
+            <span class="text-[12px] md:text-sm font-semibold text-gray-400">#${issue.author}</span>
+            <span class="text-[10px] md:text-sm font-medium text-gray-400">${new Date(issue.createdAt).toLocaleDateString()}</span>
             </div>
         </div>
 
@@ -103,11 +126,23 @@ const renderIssues = (issues) => {
 const openModal = (issue) => {
     modalTitle.textContent = issue.title;
     modalDescription.textContent = issue.description;
-    modalStatus.textContent = issue.status;
+    // status badge
+    const statusBadge = document.getElementById('modal-status-badge');
+    statusBadge.textContent = issue.status.charAt(0).toUpperCase() + issue.status.slice(1);
+    statusBadge.className = `badge ${issue.status === 'open' ? 'badge-success' : 'badge-secondary'} text-white`;
     modalAuthor.textContent = issue.author;
-    modalPriority.textContent = issue.priority;
-    modalLabel.textContent = issue.label;
-    modalCreated.textContent = new Date(issue.createdAt).toLocaleString();
+
+    // priority badge
+    modalPriority.textContent = issue.priority.toUpperCase()
+    modalPriority.className = `badge ${getPriorityColor(issue.priority)} text-white font-bold h-8 px-4`;
+    // label badge
+    modalLabel.textContent = issue.labels || "BUG";
+
+    // Date formating
+    modalCreated.textContent = new Date(issue.createdAt).toLocaleDateString();
+
+    // Assignee 
+    document.getElementById('modal-assignee').textContent = issue.author;
     document.getElementById('issue-modal').checked = true;
 }
 // tab filters
